6deb4ec59e98b1f57e66850fd88025e01cbf5377
hblancolo <hector.classicsound@gmail.com>
2016-10-18 23:55:59 +0200

    Ej 6 acabado

--- client.py
+++ client.py
@@ -10,15 +10,16 @@ import socket
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])
-print(LINE)
+METOD = sys.argv[3]
+USER = sys.argv[4]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
+    line_send = METOD + ' sip:' + USER + ' SIP/2.0\r\n\r\n'
+    print("Enviando:", line_send)
+    my_socket.send(bytes(line_send, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)  # 1024 es el valor del buffer
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
