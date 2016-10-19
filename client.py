03605783bcacba1655a9c0ad713b22e5db34f56a
hblancolo <hector.classicsound@gmail.com>
2016-10-20 01:13:26 +0200

    Actualizado segun server

--- client.py
+++ client.py
@@ -7,18 +7,22 @@ Programa cliente UDP que abre un socket a un servidor
 import sys
 import socket
 
-# Constantes. Dirección IP del servidor y contenido a enviar
-SERVER = sys.argv[1]
-PORT = int(sys.argv[2])
-METOD = sys.argv[3]
-USER = sys.argv[4]
+try:
+    SERVER = sys.argv[1]
+    PORT = int(sys.argv[2])
+    METOD = sys.argv[3].upper()  # upper me lo pone en mayusculas
+    USER = sys.argv[4]
+    EXPIRES = sys.argv[5]
+    int(EXPIRES)
+except:
+    sys.exit('Usage: client.py ip puerto register sip_address expires_value')
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    line_send = METOD + ' sip:' + USER + ' SIP/2.0\r\n\r\n'
-    print("Enviando:", line_send)
-    my_socket.send(bytes(line_send, 'utf-8') + b'\r\n')
+    line = (METOD + ' sip:' + USER + ' SIP/2.0\r\nExpires: ' + EXPIRES)
+    print("Enviando:", line)
+    my_socket.send(bytes(line, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)  # 1024 es el valor del buffer
     print('Recibido -- ', data.decode('utf-8'))
 
